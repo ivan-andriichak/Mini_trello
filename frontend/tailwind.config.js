ca09fd5efd6c7d5458efdebbfe45b599637b651c
Ivan <picea.abies10@gmail.com>
2025-09-25 16:15:29 +0300

    Added tailwind.config.ts .

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const tailwindScrollbar = require('tailwind-scrollbar');
+
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindScrollbar,
+  ],
+};

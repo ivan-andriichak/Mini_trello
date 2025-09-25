a1f722c5cb9de83dd81d1a2f5d424e91afc534dd
Ivan <picea.abies10@gmail.com>
2025-09-25 20:06:07 +0300

    Updated AuthContext.tsx, layout.tsx.

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,8 +1,8 @@
-/** @type {{plugins: string[]}} */
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
-    "tailwindcss",
-    "autoprefixer",
+    '@tailwindcss/postcss',
+    'autoprefixer',
   ],
 };
 

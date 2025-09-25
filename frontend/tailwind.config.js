dedd4252b911003126671c0745a422b506f92f22
Ivan <picea.abies10@gmail.com>
2025-09-25 19:22:32 +0300

    Updated postcss.config.mjs .

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
